429edc63f0da9180a6d00408ae5fcd8da7be4adf
danny <khc741213@gmail.com>
2022-09-28 15:17:34 +0800

    1

--- 0928/3-arrow-function.js
+++ 0928/3-arrow-function.js
@@ -28,3 +28,15 @@ console.log(sayHi('阿華','萱萱')); // 阿華萱萱你好哇
 /* 
 //如果程式碼有多行時，一定要使用大括號{}將它們括起來，並且使用 return 返回
 */
+let getYear = () => {
+    let date = new Date();
+    return date.getFullYear();
+}
+console.log(getYear()); // 2019
+
+//如果要直接 return 物件，必須在物件外面加上括號()，否則會報錯。
+// 出現錯誤
+// let myHome1 = () => { dog: 1, brother: "豪豪" };
+
+// 正確寫法
+let myHome2 = () => ({ dog: 1, brother: "豪豪" });
